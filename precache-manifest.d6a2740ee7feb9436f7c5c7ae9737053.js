862ba93e89011eee667965af1a675ce7f9f6be99
Touhidul Shawan <touhidulislamshaon@gmail.com>
2019-05-08 18:59:52 +0600

    Updates

---
+++ precache-manifest.d6a2740ee7feb9436f7c5c7ae9737053.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c73e38ac70b5f1f7fd3c9ba638295c",
+    "url": "/touhidulShawan/todo-app.git/index.html"
+  },
+  {
+    "revision": "feb7b03ac0cfa7e07e60",
+    "url": "/touhidulShawan/todo-app.git/static/css/main.5e38547a.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/touhidulShawan/todo-app.git/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "feb7b03ac0cfa7e07e60",
+    "url": "/touhidulShawan/todo-app.git/static/js/main.910faf64.chunk.js"
+  },
+  {
+    "revision": "7907de390cca913b5eff",
+    "url": "/touhidulShawan/todo-app.git/static/js/runtime~main.3c9e4f26.js"
+  }
+]);
